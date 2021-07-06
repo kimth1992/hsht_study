28fbaece5e6e9bae9369e5e6d6ec3ae23970bd15
kimth1992 <kimth1992@naver.com>
2021-07-06 20:29:04 +0900

    study 2021-07-06

---
+++ src/taeho_study/no_2557.java
@@ -0,0 +1,7 @@
+package taeho_study;
+
+public class no_2557{
+  public static void main(String[] args){
+    System.out.print("Hello World!");
+  }
+}
---
+++ src/taeho_study/no_2742.java
@@ -0,0 +1,20 @@
+package taeho_study;
+
+import java.util.Scanner;
+
+public class no_2742{
+  public static void main(String[] args){
+
+
+    Scanner sc = new Scanner(System.in);
+    int num = 0;
+    num = sc.nextInt();
+
+    for(int i=num; i>0; i--) {
+      System.out.println(i);
+    }
+
+
+  }
+
+}
