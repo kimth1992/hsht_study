2e8ea8cb29174844a9a56b5ed88e9fe1fa4696f3
woohyeongminn <gudals.woo@gmail.com>
2021-07-06 20:08:20 +0900

    study21.07.06

---
+++ src/hyungmin_study/No_1000.java
@@ -0,0 +1,23 @@
+package hyungmin_study;
+
+public class No_1000 {
+
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+
+
+    //    import java.util.Scanner;
+    //    public class Main {
+    //        public static void main(String[] args) {
+    //          Scanner scanner = new Scanner(System.in);
+    //            int a = 0;
+    //            int b = 0;
+    //            a = scanner.nextInt();
+    //            b = scanner.nextInt();
+    //            System.out.println(a+b);
+    //        }
+    //    }
+
+  }
+
+}
