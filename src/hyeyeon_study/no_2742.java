c04c375c5cd145502ea43b2fa7486146ae5a1cdb
lee-hyeyeon <lhy1415@knou.ac.kr>
2021-07-08 19:29:12 +0900

    2021-07-08

---
+++ src/hyeyeon_study/no_2742.java
@@ -0,0 +1,15 @@
+package hyeyeon_study;
+
+import java.util.Scanner;
+public class no_2742 {
+
+  public static void main(String[] args) {
+
+    Scanner scan = new Scanner(System.in);
+    int a = scan.nextInt();
+
+    for(int i = a; i > 0; i--){
+      System.out.println(i);
+    }
+  }
+}
