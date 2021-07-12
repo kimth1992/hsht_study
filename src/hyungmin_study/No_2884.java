a530ba73a5b958efdf3f4416bdf612f5203f135f
woohyeongminn <gudals.woo@gmail.com>
2021-07-12 23:32:50 +0900

    2021-07-12

--- src/hyungmin_study/No_2884.java
+++ src/hyungmin_study/No_2884.java
@@ -1,27 +1,21 @@
 package hyungmin_study;
-
 import java.util.Scanner;
-
-//실행은 되나 런타임 에러가 뜹니다.
-
 public class No_2884 {
-
+  //정답아님  런타임 오류 
   public static void main(String[] args) {
     Scanner scn = new Scanner(System.in);
     int h = Integer.parseInt(scn.nextLine());
     int m = Integer.parseInt(scn.nextLine());
-
-
     if (m>=45) {
       System.out.print(h + " ");
       System.out.println(m-45);
-    }else if (h==0) {
-      System.out.print(23 + " ");
-      System.out.println(m+15);
-    }else if (h>=1 && h<=23) {
-      System.out.print(h-1 + " ");
+    }else { 
+      if(h==0) {
+        System.out.print(23 + " ");
+        System.out.println(m+15);
+      }else 
+        System.out.print(h-1 + " ");
       System.out.println(m+15);
     }
-
   }
 }
