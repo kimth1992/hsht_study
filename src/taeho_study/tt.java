4eb66f6be17079ef0f562e139f8c8edce7497f4a
kimth1992 <kimth1992@naver.com>
2021-11-27 23:04:38 +0900

    .

--- src/taeho_study/tt.java
+++ src/taeho_study/tt.java
@@ -1,28 +1,20 @@
 package taeho_study;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class tt {
+
+  int a = 1;
+
+  void add() {
+    int a = 2;
+  }
+
 
   public static void main(String[] args) {
     // TODO Auto-generated method stub
+    tt ok = new tt();
 
-    List<String> list = new ArrayList<>();
-    list.add("하이");
-    list.add("ok");
-    list.add("kkkkk");
-    list.add("last");
-    System.out.println("size = " + list.size());
-
-    for (int i = list.size() - 1; i >= 0; i--) {
-      System.out.printf("사이즈는 %d, 첫번째 i는 %d\n",list.size(),i);
-      list.remove(i);
-      System.out.println();
-      break;
-    }
-
-    System.out.println(list);
+    int a = 1;
+    ok.add();
 
   }
 
