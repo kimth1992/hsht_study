3d4b48d13f9db20377b52e2a2fb6f25c93afd730
kimth1992 <kimth1992@naver.com>
2021-11-29 20:16:34 +0900

    solve problem

--- src/taeho_study/baekjoon/no_10000.java
+++ src/taeho_study/baekjoon/no_10000.java
@@ -2,4 +2,15 @@ package taeho_study.baekjoon;
 
 public class no_10000 {
 
+  static int a = 100;
+  int b = 200;
+
+
+  public static void main(String[] args) {
+
+    a = 100;
+    b = 200;
+
+  }
+
 }
